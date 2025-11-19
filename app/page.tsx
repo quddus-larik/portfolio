fd1f399ee7b8349eb45ec004e1fbd5379a76b4f0
quddus-larik <quddus@proton.me>
2025-11-20 00:10:07 +0500

    Update UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,50 +1,24 @@
 "use client";
-import { useRef } from 'react';
+
+import { Lenis } from "lenis/react"
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
-import { useGSAP } from '@gsap/react';
-import { motion } from 'framer-motion';
-import { Navbar } from '@/components/custom/navbar';
-import { GridBackground } from '@/components/custom/backeground'; // Removed unused DotBackground
-import { EvervaultCard, Icon } from '@/components/ui/evervault-card';
-import { SiDribbble, SiGithub, SiNextdotjs, SiReact, SiTypescript } from 'react-icons/si';
-import { RevealText } from '@/components/custom/copy';
+import { SiDribbble, SiGithub } from 'react-icons/si';
+import { RevealText } from '@/components/custom/revealtext';
 // Removed unused Spotlight
 import BounceCards from '@/components/BounceCards';
 import Shuffle from '@/components/Shuffle';
 import { ScaleRotateBox } from '@/components/custom/animateonscroll';
 import PixelBlast from '@/components/PixelBlast';
-import { LinkPreview } from '@/components/ui/link-preview';
-import GlassSurface from '@/components/GlassSurface';
 import { Button } from '@/components/ui/button';
 import { RainbowButton } from '@/components/ui/rainbow-button';
-import { ArrowDown } from 'lucide-react';
-import { TextRoll } from '@/components/ui/text-roll';
+import MagnetLines from "@/components/MagnetLines";
+import { Star } from "lucide-react";
+import { Lens } from "@/components/ui/lens";
 gsap.registerPlugin(ScrollTrigger);
 
 export default function Home() {
 
-    const containerRef = useRef<HTMLDivElement>(null);
-
-    const numberOfSections = 3;
-
-    useGSAP(() => {
-        const sections = gsap.utils.toArray<HTMLElement>(".horizontal-section");
-
-        if (!containerRef.current) return; // Add check for null ref
-
-        gsap.to(sections, {
-            xPercent: -100 * (numberOfSections - 1),
-            ease: "power2.out",
-            scrollTrigger: {
-                trigger: containerRef.current,
-                pin: true,
-                scrub: 1,
-                end: () => `+=${containerRef.current?.offsetWidth || 0}`, // Use template literal for cleaner code
-                invalidateOnRefresh: true,
-            },
-        });
-    }, { scope: containerRef });
 
     const images = [
         "/images/mongo.png",
@@ -65,13 +39,14 @@ export default function Home() {
 
 
     return (
-        <div className='overflow-x-hidden'>
 
-            <div className="h-svh w-svw bg-gray-50 flex justify-center items-end">
+        <Lenis root className='overflow-hidden'>
+
+            <div className="h-svh w-full bg-gray-50 flex justify-center items-end">
                 <PixelBlast
                     variant="square"
                     pixelSize={4}
-                    color="#99A1AF"
+                    color="#030712"
                     patternScale={2}
                     patternDensity={1}
                     pixelSizeJitter={0}
@@ -103,12 +78,11 @@ export default function Home() {
                         <p className='w-fit flex items-center justify-center gap-1 h-10 px-6 border rounded-full bg-slate-100/10 backdrop-blur-md border-white/10 font-clash font-medium tracking-wider'>I specialize in crafting full-stack MERN applications that turn ideas into reliable, intuitive, and goal-oriented web products.</p>
                     </RevealText>
 
-                    <RainbowButton className='rounded-full min-w-8 font-clash tracking-wider'>My Journey</RainbowButton>
                 </div>
             </div>
 
-            <div ref={containerRef} className="flex h-svh w-full">
-                <div className="min-h-svh w-svw bg-gray-50 shrink-0 gap-2 flex-col horizontal-section flex p-5" >
+            <div className="flex h-svh w-full">
+                <div className="min-h-svh w-svw bg-gray-50  gap-2 flex-col  flex p-5" >
                     <div className="grid grid-cols-4 grid-rows-4 gap-4 w-full h-full">
                         <div className="col-span-5 row-span-2 w-full bg-transparent grid place-content-center">
                             <BounceCards
@@ -137,8 +111,8 @@ export default function Home() {
                                 respectReducedMotion={true}
                                 fontSize='4rem'
                             />
-                            <RevealText duration={1}>
-                                <div className='font-clash font-medium text-xl text-left'>Skilled in <LinkPreview url='https://mongodb.com'>MongoDB</LinkPreview>, <LinkPreview url='https://expressjs.com'>Express.js</LinkPreview>, <LinkPreview url='reactjs.com'>React</LinkPreview>, and <LinkPreview url='https://nodejs.com'>Node.</LinkPreview> with a strong commitment to building intuitive user experiences and writing efficient, maintainable code. Dedicated to turning ideas into real-world digital solutions and always eager to collaborate, learn, and grow through impactful projects.</div>
+                            <RevealText duration={1} >
+                                <div className='font-clash font-medium text-xl text-left'>Skilled in  MongoDB, Express.js, React, and Node. with a strong commitment to building intuitive user experiences and writing efficient, maintainable code. Dedicated to turning ideas into real-world digital solutions and always eager to collaborate, learn, and grow through impactful projects.</div>
                             </RevealText>
                             <div className='w-full flex gap-3'>
                                 <Button className='font-clash font-medium rounded-none tracking-wider'><SiGithub />Github</Button>
@@ -147,97 +121,39 @@ export default function Home() {
                         </div>
 
                         <div className="row-span-2 col-start-1 row-start-3 w-full bg-transparent grid place-content-center ">
-                            <ScaleRotateBox />
+                            <ScaleRotateBox className='h-40 w-40 bg-black' />
                         </div>
                     </div>
                 </div>
+            </div>
+            <div className="relative">
+                <div className="sticky top-0 h-svh flex flex-col items-start justify-start bg-gray-100 px-8 py-3">
+                    <div className="w-[200px] h-8 text-white bg-gray-900 my-2 text-2xl"/>
+                    <RevealText duration={1}>
+                        <div className="text-8xl font-akira text-left">I CREATE EXPERIENCES NOT JUST WEBSITES!</div>
+                    </RevealText>
 
-                {/* MODIFIED: Set viewport={{ once: false }} */}
-                <div className="h-svh w-svw bg-gray-50/80 shrink-0 horizontal-section flex flex-col p-5" >
-                    <TextRoll
-                        className='text-8xl font-akira'
-                        whileInView
-                        animateOnHover
-                    >FULL STACK ENGINEER</TextRoll>
-                    <Shuffle
-                        text="{ PROJECTS }"
-                        shuffleDirection="right"
-                        duration={0.50}
-                        animationMode="evenodd"
-                        shuffleTimes={1}
-                        ease="power3.out"
-                        stagger={0.03}
-                        threshold={0.1}
-                        triggerOnce={false}
-                        triggerOnHover={true}
-                        respectReducedMotion={true}
-                        fontSize='2rem'
-                        font='Clash'
-                        style={{ fontWeight: "bold" }}
-                    />
-
-                    <div className='w-full h-full grid place-content-center'>
-                        <ScaleRotateBox />
-                    </div>
                 </div>
+                <div className="sticky top-13 h-svh flex gap-2 flex-col items-start justify-start bg-gray-200 px-8 py-3">
+                    <div className="w-[200px] h-10 text-white bg-gray-900 my-2 ml-3 text-2xl"/>
+                    <RevealText duration={1}>
+                        <div className="text-8xl font-akira text-left">PROJECTS</div>
+                    </RevealText>
+                    
 
-                {/* MODIFIED: Set viewport={{ once: false }} */}
-                <div className="h-svh w-svw bg-green-300 shrink-0 horizontal-section flex items-center justify-center" >
-                    <motion.p
-                        className='font-akira'
-                        initial={{ fontSize: "32px", rotate: "10deg" }}
-                        whileInView={{ fontSize: "120px", rotate: "0deg" }}
-                        viewport={{ once: false }}
-                        transition={{ duration: 0.70 }}
-                    >
-                        Whoo!
-                    </motion.p>
                 </div>
-
+                <div className="sticky top-0 h-screen flex flex-col items-center justify-center bg-purple-600 text-white">
+                    <h2 className="text-4xl">The Third Title</h2>
+                    <p>Scroll Down</p>
+                </div>
+                <div className="sticky top-0 h-screen flex flex-col items-center justify-center bg-neutral-800 text-white">
+                    <h2 className="text-4xl">The Fourth Title</h2>
+                </div>
             </div>
 
-            <div className="h-svh w-svw bg-purple-300" >
-                <div className='h-svh w-svw shrink-0 horizontal-section flex items-center justify-center gap-2'>
-                    <div className="border border-black/[0.2] dark:border-white/[0.2] flex flex-col items-start max-w-sm mx-auto p-4 relative h-[30rem]">
-                        <Icon className="absolute h-6 w-6 -top-3 -left-3 dark:text-white text-black" />
-                        <Icon className="absolute h-6 w-6 -bottom-3 -left-3 dark:text-white text-black" />
-                        <Icon className="absolute h-6 w-6 -top-3 -right-3 dark:text-white text-black" />
-                        <Icon className="absolute h-6 w-6 -bottom-3 -right-3 dark:text-white text-black" />
 
-                        <EvervaultCard object={<SiNextdotjs className='text-black dark:text-white text-6xl' />} from='black' />
 
-                        <h2 className="dark:text-white text-black mt-4 text-lg font-pops">
-                            I Build Creative UX focused UI using Reactjs. I primerly use javascript to build Amazings things.
-                        </h2>
-                    </div>
 
-                    <div className="border border-black/[0.2] dark:border-white/[0.2] flex flex-col items-start max-w-sm mx-auto p-4 relative h-[30rem]">
-                        <Icon className="absolute h-6 w-6 -top-3 -left-3 dark:text-white text-black" />
-                        <Icon className="absolute h-6 w-6 -bottom-3 -left-3 dark:text-white text-black" />
-                        <Icon className="absolute h-6 w-6 -top-3 -right-3 dark:text-white text-black" />
-                        <Icon className="absolute h-6 w-6 -bottom-3 -right-3 dark:text-white text-black" />
-
-                        <EvervaultCard object={<SiTypescript className='text-sky-700 text-6xl' />} from='blue' />
-
-                        <h2 className="dark:text-white text-black mt-4 text-lg font-pops">
-                            I Build Creative UX focused UI using Reactjs. I primerly use javascript to build Amazings things.
-                        </h2>
-                    </div>
-
-                    <div className="border border-black/[0.2] dark:border-white/[0.2] flex flex-col items-start max-w-sm mx-auto p-4 relative h-[30rem]">
-                        <Icon className="absolute h-6 w-6 -top-3 -left-3 dark:text-white text-black" />
-                        <Icon className="absolute h-6 w-6 -bottom-3 -left-3 dark:text-white text-black" />
-                        <Icon className="absolute h-6 w-6 -top-3 -right-3 dark:text-white text-black" />
-                        <Icon className="absolute h-6 w-6 -bottom-3 -right-3 dark:text-white text-black" />
-
-                        <EvervaultCard object={<SiReact className='text-sky-600 text-6xl' />} />
-
-                        <h2 className="dark:text-white text-black mt-4 text-lg font-pops">
-                            I Build Creative UX focused UI using Reactjs. I primerly use javascript to build Amazings things.
-                        </h2>
-                    </div>
-                </div>
-            </div>
-        </div>
+        </Lenis>
     );
 }
